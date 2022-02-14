8b523dac30a8a2bcb576f35b5ea855c8003b91d7
Justen Di Ruscio <justend29@gmail.com>
2022-02-14 11:25:04 -0400

    correctly configure amplify in an app widget

--- README.md
+++ README.md
@@ -14,3 +14,9 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter, view our
 [online documentation](https://flutter.dev/docs), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+
+## Developing
+
+> Work in progress - just tentative now
+
+Get dependencies specified in *pubspec.yaml*: `flutter pub get`
--- lib/main.dart
+++ lib/main.dart
@@ -7,72 +7,41 @@ import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:parkr/amplifyconfiguration.dart';
 import 'package:parkr/views/welcomepage.dart';
 
-class ParkrApp extends StatelessWidget {
+class ParkrApp extends StatefulWidget {
   const ParkrApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Parkr',
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-      ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
   static const String title = 'replace with one from AWS Cognito';
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<ParkrApp> createState() => _ParkrAppState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _ParkrAppState extends State<ParkrApp> {
   @override
   initState() {
     super.initState();
     _configureAmplify();
   }
 
-  void _configureAmplify() async {
+  Future<void> _configureAmplify() async {
     // Add Pinpoint and Cognito Plugins, or any other plugins you want to use
-    AmplifyAnalyticsPinpoint analyticsPlugin = AmplifyAnalyticsPinpoint();
-    AmplifyAuthCognito authPlugin = AmplifyAuthCognito();
-    await Amplify.addPlugins([authPlugin, analyticsPlugin]);
-
-    // Once Plugins are added, configure Amplify
-    // Note: Amplify can only be configured once.
+    // Add all plugins before configuring Amplify
+    // Amplify.configure() may only be called once
     try {
-      await Amplify.configure(amplifyconfig);
-    } on AmplifyAlreadyConfiguredException {
+      await Amplify.addPlugins(
+          [AmplifyAnalyticsPinpoint(), AmplifyAuthCognito()]);
+      await Amplify.configure(amplifyconfig); // from amplifyconfiguration.dart
+    } on Exception catch (e) {
       print(
-          "Tried to reconfigure Amplify; this can occur when your app restarts on Android.");
+          "Tried to reconfigure Amplify; this can occur when your app restarts on Android. $e");
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const MyHomePage()),
-                  );
-                },
-                child: const Text('Login', style: TextStyle(fontSize: 20.0)))
-          ],
-        ),
-      ),
+    return MaterialApp(
+      title: 'Parkr',
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: Scaffold(body: Container()),
     );
   } // build
 }
