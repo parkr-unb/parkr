8d21a2eb01992d4d9e1748b430a5046f20d7e418
Justen Di Ruscio <justend29@gmail.com>
2022-03-17 15:17:12 -0300

    add query and schema for app keys

--- lib/gateway.dart
+++ lib/gateway.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:amplify_api/amplify_api.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:parkr/models/ModelProvider.dart';
@@ -17,40 +19,36 @@ class Gateway {
     // TODO: also send an email to parker
     final licenseOrg = license.trim().replaceAll("-", "") + "-" + "unb";
     try {
-      final request = ModelQueries.get(
-          Tickets.classType, licenseOrg);
-      final response = await Amplify.API
-          .query(request: request)
-          .response;
+      final request = ModelQueries.get(Tickets.classType, licenseOrg);
+      final response = await Amplify.API.query(request: request).response;
       Tickets? tickets = response.data;
-      if (tickets == null) { // No tickets associated with license plate, create a row
+      if (tickets == null) {
+        // No tickets associated with license plate, create a row
         print('errors (response): ' + response.errors.toString());
-        final Ticket newTicket = Ticket(
-            createdAt: TemporalDateTime.now(),
-            type: ticketType);
-        final List<Ticket> newTicketList = List.filled(1, newTicket, growable: true);
-        final Tickets newTickets = Tickets(
-            id: licenseOrg, tickets: newTicketList);
+        final Ticket newTicket =
+            Ticket(createdAt: TemporalDateTime.now(), type: ticketType);
+        final List<Ticket> newTicketList =
+            List.filled(1, newTicket, growable: true);
+        final Tickets newTickets =
+            Tickets(id: licenseOrg, tickets: newTicketList);
         final createRequest = ModelMutations.create(newTickets);
-        final createResponse = await Amplify.API
-            .mutate(request: createRequest)
-            .response;
+        final createResponse =
+            await Amplify.API.mutate(request: createRequest).response;
         if (createResponse.data == null) {
           print('errors (createResponse): ' + createResponse.errors.toString());
         }
         return createResponse.data;
       }
       // Tickets found with license plate, add ticket to Tickets list
-      final Ticket newTicket = Ticket(
-          createdAt: TemporalDateTime.now(),
-          type: ticketType);
+      final Ticket newTicket =
+          Ticket(createdAt: TemporalDateTime.now(), type: ticketType);
       tickets.tickets?.add(newTicket);
       final mutationRequest = ModelMutations.update(tickets);
-      final mutationResponse = await Amplify.API
-          .mutate(request: mutationRequest)
-          .response;
+      final mutationResponse =
+          await Amplify.API.mutate(request: mutationRequest).response;
       if (mutationResponse.data == null) {
-        print('errors (mutationResponse): ' + mutationResponse.errors.toString());
+        print(
+            'errors (mutationResponse): ' + mutationResponse.errors.toString());
       }
       return mutationResponse.data;
     } on ApiException catch (e) {
@@ -91,6 +89,7 @@ class Gateway {
       rethrow;
     }
   }
+
   Future<Organization?> addOrganization(String orgId) async {
     try {
       final org = Organization(id: orgId);
@@ -106,6 +105,7 @@ class Gateway {
       rethrow;
     }
   }
+
   Future<Officer?> getOfficerByID(String id) async {
     try {
       final request = ModelQueries.get(Officer.classType, id);
@@ -139,13 +139,24 @@ class Gateway {
     }
   }
 
-  Future<ParkingPermits?> createParkingPermit(String license, String firstName, String lastName, String email) async {
+  Future<ParkingPermits?> createParkingPermit(
+      String license, String firstName, String lastName, String email) async {
     // Only used for inputting passes into database manually
     // Would use existing system to enter passes in real world
-    final ParkingPermit pass = ParkingPermit(termStart: TemporalDateTime(DateTime.parse("2022-02-10T20:03:33.604760000Z")), termEnd: TemporalDateTime(DateTime.parse("2022-02-10T20:03:33.604760000Z")), passType: 'student');
+    final ParkingPermit pass = ParkingPermit(
+        termStart:
+            TemporalDateTime(DateTime.parse("2022-02-10T20:03:33.604760000Z")),
+        termEnd:
+            TemporalDateTime(DateTime.parse("2022-02-10T20:03:33.604760000Z")),
+        passType: 'student');
     final List<ParkingPermit> passes = List.filled(1, pass, growable: true);
     try {
-      ParkingPermits permits = ParkingPermits(id: '$license-unb', permits: passes, firstName: firstName, lastName: lastName, emailAddress: email);
+      ParkingPermits permits = ParkingPermits(
+          id: '$license-unb',
+          permits: passes,
+          firstName: firstName,
+          lastName: lastName,
+          emailAddress: email);
       final request = ModelMutations.create(permits);
       final response = await Amplify.API.mutate(request: request).response;
 
@@ -164,8 +175,7 @@ class Gateway {
 
   Future<ParkingPermits?> queryParkingPermits(String license) async {
     try {
-      final request = ModelQueries.get(
-          ParkingPermits.classType, license);
+      final request = ModelQueries.get(ParkingPermits.classType, license);
       final response = await Amplify.API.query(request: request).response;
       ParkingPermits? passes = response.data;
       if (passes == null) {
@@ -174,6 +184,28 @@ class Gateway {
       }
       print("Query: " + (passes.permits?.elementAt(0)?.toString() ?? ''));
       return passes;
+    } on ApiException catch (e) {
+      print('Query failed: $e');
+    }
+    return null;
+  }
+
+  AppKeys? cachedKeys;
+
+  Future<AppKeys?> queryAppKeys({useCached = true}) async {
+    if (cachedKeys != null && useCached == true) {
+      return cachedKeys;
+    }
+    try {
+      final request = ModelQueries.get(AppKeys.classType, "shared");
+      final response = await Amplify.API.query(request: request).response;
+      AppKeys? appKeys = response.data;
+      if (appKeys == null) {
+        print('errors: ' + response.errors.toString());
+        return null;
+      }
+      cachedKeys = appKeys;
+      return appKeys;
     } on ApiException catch (e) {
       print('Query failed: $e');
     }
